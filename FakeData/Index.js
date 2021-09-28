c9c966a6be0075fbe2ecc2d860284b78914e0c9a
Al Nasimul Haque <alnasimul@gmail.com>
2021-09-29 01:26:36 +0600

    service detail page added

--- FakeData/Index.js
+++ FakeData/Index.js
@@ -4,41 +4,94 @@ export const data = [
         title: 'A/C Services',
         description: 'The air conditioning system or your car is a sealed system. In closed systems the only way you lose fluid is when you have a leak in the system. Having the air conditioning evaluated and recharged about every three is a good maintenance guideline, even if it seems to be working well.',
         excerpt: 'The air conditioning system or your car is a sealed system. In closed systems the only way you lose fluid is when you have a leak...',
-        image: '/images/Services/AC Service.jpg'
+        image: '/images/Services/AC Service.jpg',
+        services: [
+            'System fan level check',
+            'System PSI pressure reading to verify Freon level',
+            'Output temperature check',
+            'Blower fan and compressor examination to ensure proper operation',
+            'Belts and hoses inspection to discover indications of aging and potential failure',
+            'Diagnosis of possible problems with faulty switches, fuses, wiring, the condenser, or internal compressor seal failure'
+        ],
+        price: 350
     },
     {   
         slug: 'brake-repair',
         title: 'Brake Repair',
         description: 'When it comes to brakes you shouldn’t wait until brake repairs are needed. If the owner continues to drive the vehicle while there are issues with the brakes, then the chance of having an accident can increase or unpleasant vibrations and loud screeching sounds can be experienced while driving.',
         excerpt: 'When it comes to brakes you shouldn’t wait until brake repairs are needed. If the owner continues to drive the vehicle while...',
-        image: '/images/Services/Brake Repair.jpg'
+        image: '/images/Services/Brake Repair.jpg',
+        services: [
+            'Brake pads replacement',
+            'Brake rotors / drums resurfaced',
+            'Brake fluid flush & replacement',
+            'Brake caliper & rotor re-alignment',
+            'Brake caliper replacement',
+            'Brake hose replacement',
+            'Brake master cylinder replacement'
+        ],
+        price: 300
     },
     {   
         slug: 'engine-repair',
         title: 'Engine Repair',
         description: 'If your engine isn’t working properly chances are you’re in for a costly repair. An engine replacement can set you thousands behind.With preventative maintenance you can keep your car’s engine running smoothly. Your engine is a complex system of electrical systems, complicated mechanical systems and they all operate under high temperatures to keep you going down the road.',
         excerpt: 'If your engine isn’t working properly chances are you’re in for a costly repair. An engine replacement can set you thousands behind.With preventative maintenance...',
-        image: '/images/Services/Engine Repair.jpg'
+        image: '/images/Services/Engine Repair.jpg',
+        services: [
+            'Computerized Diagnostics',
+            'Complete Engine Tune-ups',
+            'Intake Manifold Replacement',
+            'Head Gasket Replacement',
+            'Complete Visual Inspection of Engine',
+            'Oil Leak Diagnosis',
+            'Spark Plug Replacement',
+            'Valve Cover Gasket Replacement',
+            'Ignition Coil Replacement'
+        ],
+        price: 1200
     },
     {
         slug: 'oil-change',
         title: 'Oil Change',
         description: 'Your vehicle is one of the biggest investments you’ll make. With preventative maintenance you can keep it running smoothly and avoid potential engine damage along with costly repairs. Oil changes are the single most important regular maintenance item for your vehicle.',
         excerpt: 'Your vehicle is one of the biggest investments you’ll make. With preventative maintenance you can keep it running smoothly and avoid potential engine damage along...',
-        image: '/images/Services/Engine Oil.jpg'
+        image: '/images/Services/Engine Oil.jpg',
+        price: 50,
     },
     {
-        slug: 'tire-change',
-        title: 'Tire Change',
+        slug: 'tire-repair',
+        title: 'Tire Repair',
         description: 'Generally speaking, tires should be rotated every 6,000 to 8,000 miles. A technician can thoroughly inspect your tires as well as your wheels to determine if they have an acceptable amount of wear. The technician can also check the tread depth and ensure they are properly balanced.',
         excerpt: 'Generally speaking, tires should be rotated every 6,000 to 8,000 miles. A technician can thoroughly inspect your tires as well as your wheels to determine...',
-        image: '/images/Services/Tire Change.jpg'
+        image: '/images/Services/Tire Change.jpg',
+        services: [
+            'Dismounting and inspection of the tire to determine the cause of the inflation pressure loss and whether it can be repaired',
+            'Flat tire repair',
+            'Tire repair will be performed in accordance with strict industry standards',
+            'If suitable for the repair, proper plugging and patching from the inside of the tire will be performed',
+            'Rebalancing of tire and wheel assembly',
+            'Reactivating of the tire pressure monitoring system (if applicable)'
+        ],
+        price: 50
     },
     {
         slug: 'performance',
         title: 'Performance',
         description: 'We offer a broad range of performance products from engine, driveline and chassis tuning components. These fully re-tuned products maximize power and performance while keeping all the car factory safeguards in place.',
         excerpt: 'We offer a broad range of performance products from engine, driveline and chassis tuning components. These fully re-tuned products maximize power and performance...',
-        image: '/images/Services/Engine Performance.jpg'
+        image: '/images/Services/Engine Performance.jpg',
+        services: [
+            'Computerized Diagnostics',
+            'Complete Engine Tune-ups',
+            'Intake Manifold Replacement',
+            'Head Gasket Replacement',
+            'Complete Visual Inspection of Engine',
+            'Oil Leak Diagnosis',
+            'Spark Plug Replacement',
+            'Valve Cover Gasket Replacement',
+            'Ignition Coil Replacement'
+        ],
+        price: 1400
     }
 ]
--- pages/services/[slug].js
+++ pages/services/[slug].js
@@ -1,9 +1,64 @@
-const ServicePage = () => {
+import Layout from "@/components/Layout";
+import Link from "next/link";
+import { data } from "FakeData/Index";
+
+const ServicePage = ({ service }) => {
+    const { title, description, image, price, services } = service;
     return (
-        <div>
-            Enter
-        </div>
+        <Layout title={title}>
+            <Link href='/'>
+                <a className='text-gray-900 hover:text-blue-600 ml-9 md:mx-5'>
+                  {"<<"}  Go Back
+                </a>
+            </Link>
+            <div className='w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6'>
+                <div className="flex justify-between items-center mt-4">
+                <h1 className="text-xl font-bold mb-7 md:text-5xl">{title}</h1>
+                    <p className='mb-5'><span className='bg-purple-600 text-white lowercase font-bold pt-1 pb-2 px-2 rounded text-sm '>Powered by Auto Lane</span></p>
+                </div>
+                <div className='block justify-between md:flex'>
+                    <img src={image} alt="" className='w-full rounded'/>
+                    <div className='m-5'>
+                        
+                        <p className="text-justify text-gray-600 font-bold">{description}</p>
+                        {
+                            services && <div className='mt-5'>
+                                  <h3 className='text-2xl font-bold text-gray-600'>Services we are providing</h3>
+                                  <ul className="mt-3 ml-5">
+                                      {services.map((service, index) => 
+                                          <li key={index} className='mb-3 text-gray-600'>{service}</li>
+                                      )}
+                                  </ul>
+                            </div>
+                        }
+                        <p className="text-red-500 mt-5 font-bold">Pricing: $ {price}</p>
+                    </div>
+                </div>
+            </div>
+        </Layout>
     );
+}
+
+export const getStaticPaths = async () => {
+    const services = data;
+
+    const paths = services.map(service => ({
+        params: { slug: service.slug }
+    }))
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps = async ({ params: { slug } }) => {
+
+    const service = data.find(singleData => singleData.slug === slug);
+
+    return {
+        props: { service }
+    }
 }
 
 export default ServicePage;
