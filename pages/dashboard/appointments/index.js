47ce70ee4f5017ad1724a060673f22286fdd07b7
Al Nasimul Haque <alnasimul@gmail.com>
2022-01-10 22:32:09 +0600

    bug fixed

--- pages/dashboard/appointments/index.js
+++ pages/dashboard/appointments/index.js
@@ -2,9 +2,8 @@ import moment from "moment";
 import AdminPanelLayout from "@/components/AdminPanelLayout";
 import Appointments from "@/components/Appointments/Appointments";
 import autolaneApi from "pages/api/autolane";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { withAdmin } from "@/helpers/route";
 
 const AppointmentsPage = ({ appointments }) => {
   const [selectedDate, setSelectedDate] = useState(new Date());
@@ -14,6 +13,7 @@ const AppointmentsPage = ({ appointments }) => {
  
 
   const router = useRouter();
+
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
